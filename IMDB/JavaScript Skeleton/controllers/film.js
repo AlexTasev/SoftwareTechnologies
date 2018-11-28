4bd341eef9128d58becb56191dea288a716a7d2b
Aleksandar Tasev <40795644+AlexTasev@users.noreply.github.com>
2018-11-28 16:56:17 +0200

    Add files via upload

---
+++ IMDB/JavaScript Skeleton/controllers/film.js
@@ -0,0 +1,67 @@
+const Film = require('../models').Film;
+
+module.exports = {
+    index: (req, res) => {
+        Film.findAll().then(films => {
+            res.render("film/index", {"films": films});
+        })
+    },
+
+    createGet: (req, res) => {
+        res.render("film/create")
+    },
+
+    createPost: (req, res) => {
+        let body = req.body;
+        Film.create(body).then(()=> {
+            res.redirect("/")
+        });
+    },
+
+    editGet: (req, res) => {
+        let id = req.params.id;
+
+        Film.findById(id).then(film => {
+            res.render("film/edit", {"name": film.name,
+                "genre": film.genre,
+                "director": film.director,
+                "year": film.year,
+                "id": id
+            });
+        })
+    },
+
+    editPost: (req, res) => {
+        let id = req.params.id;
+        let body = req.body;
+
+        Film.findById(id).then(film => {
+            film.updateAttributes(body).then(() => {
+                res.redirect("/")
+            })
+        })
+    },
+
+    deleteGet: (req, res) => {
+        let id = req.params.id;
+
+        Film.findById(id).then(film => {
+            res.render("film/delete", {"name": film.name,
+                "genre": film.genre,
+                "director": film.director,
+                "year": film.year,
+                "id": id
+            });
+        })
+    },
+
+    deletePost: (req, res) => {
+        let id = req.params.id;
+
+        Film.findById(id).then(film => {
+            film.destroy({force:true}).then(() => {
+                res.redirect("/");
+            })
+        });
+    }
+};
